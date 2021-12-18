d20a909a0693341ae05d1928830b26f4dcc6dbaa
AriellaKatz <akatz30@stuy.edu>
2021-12-18 18:45:44 -0500

    lab03

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # ABA - Ariella Katz, Alif Rahman, Brianna Tieu, Tom, Grippy, Pepe
+## Tests Performed
+Created 4 arrays total of lengths 500,000, 1,000,000, 5,000,000, and 10,000,000, containing consecutive multiples of 2 starting with 0.
+Conducted 100 binary searches and 100 linear searches of each array for a random integer between 0 and twice the array length (0 and the maximum value in the array). 
+This guaranteed that the chance of the target not being in the array would be the same for arrays of all lengths (50%, since there would be a 50% chance of the target being odd). This way, the results would not be skewed due to the target not being found more frequently for shorter array lengths - such an error would result in deceptively longer average times for shorter arrays.
+For each search, the elapsed time was found by subtracting the timestamp directly before the search from the timestamp directly after the search. Then, the average elapsed time for each set of 100 searches was found.
+We displayed the average elapsed times for the binary searches and linear searches for each array, as well as the difference between the average elapsed times for binary search and linear search for each array. 
+
 ## Results
+For the 500,000-length, 1,000,000-length, 5,000,000-length, and 10,000,000-length arrays, the average elapsed times for linear search were 0, 1, 5, and 11 seconds, respectively, and the average elapsed times for binary search were 0, 0, 0, and 0 seconds, respectively.
+The difference between the average elapsed times for linear search and binary search were 0, 1, 6, and 12 seconds, respectively.
+Repetitions of this experiment always yielded elapsed times of 0 seconds for both linear and binary search for the shortest array (500,000) and elapsed times of 0 seconds for binary search for all arrays.
+(Thus the difference between elapsed times for linear and binary search for the shortest array was always 0 seconds.)
+The average elapsed times for linear search of the other 3 arrays never differed from these results by more than 1 second, and the difference between the average elapsed times for linear and binary search for the other 3 arrays also never differed from these results by more than 1 second.
 
 ## Conclusions
+Binary search appears to be more efficient than linear search, as it wass always faster than linear search unless both take 0 seconds. 
+The difference in efficiency appears to become more pronounces as the length of the array being searched increases.
+Moreover, binary search does not appear to decrease in efficiency by a significant amount as the array length increases, at least up to lengths that the system's memory can handle, since it took approximately 0 seconds for all array lengths.
--- SearchDriver.java
+++ SearchDriver.java
@@ -1,3 +1,82 @@
 public class SearchDriver {
 
+  public static long avgTimeBin(Comparable[] iArrX) {
+    long start;
+    long end;
+    long timeElapsed;
+    int sum = 0;
+    for (int x = 0; x < 100; x++) {
+	      start = System.currentTimeMillis();
+	      BinSearch.binSearch(iArrX, (int)(Math.random() * iArrX.length * 2));
+	      end = System.currentTimeMillis();
+	      timeElapsed = end - start;
+	      sum += timeElapsed;
+     }
+     long avgTimeBin = sum/100;
+     return avgTimeBin;
+  }
+
+  public static long avgTimeLin(Comparable[] iArrX) {
+    long start;
+    long end;
+    long timeElapsed;
+    int sum = 0;
+    for (int x = 0; x < 100; x++) {
+	      start = System.currentTimeMillis();
+	      LinSearch.linSearch(iArrX, (int)(Math.random() * iArrX.length * 2));
+	      end = System.currentTimeMillis();
+	      timeElapsed = end - start;
+	      sum += timeElapsed;
+     }
+     long avgTimeLin = sum/100;
+     return avgTimeLin;
+  }
+
+  public static void main(String[] args) {
+    //Making 4 different arrays w/ diff lengths
+    Comparable[] iArr0 = new Integer[500000];
+    for (int i = 0; i < iArr0.length; i++) {
+	     iArr0[i] = i*2;
+    }
+    Comparable[] iArr1 = new Integer[1000000];
+    for (int i = 0; i < iArr1.length; i++) {
+       iArr1[i] = i*2;
+    }
+    Comparable[] iArr2 = new Integer[5000000];
+    for (int i = 0; i < iArr2.length; i++) {
+	     iArr2[i] = i*2;
+    }
+    Comparable[] iArr3 = new Integer[10000000];
+    for (int i = 0; i < iArr3.length; i++) {
+	     iArr3[i] = i*2;
+    }
+
+    //Finding time diff for arr.length = 100000
+    System.out.println("Average Time for BinSearch w/ length 500000: " + avgTimeBin(iArr0));
+    System.out.println("Average Time for LinSearch w/ length 500000: " + avgTimeLin(iArr0));
+    System.out.println("Diff btwn Bin and Lin w/ length 500000: " + (avgTimeLin(iArr0)-avgTimeBin(iArr0)));
+
+    System.out.println("-------------------------------------------------");
+
+    //Finding time diff for arr.length = 500000
+    System.out.println("Average Time for BinSearch w/ length 1000000: " + avgTimeBin(iArr1));
+    System.out.println("Average Time for LinSearch w/ length 1000000: " + avgTimeLin(iArr1));
+    System.out.println("Diff btwn Bin and Lin w/ length 1000000: " + (avgTimeLin(iArr1)-avgTimeBin(iArr1)));
+
+    System.out.println("-------------------------------------------------");
+
+    //Finding time diff for arr.length = 100000
+    System.out.println("Average Time for BinSearch w/ length 5000000: " + avgTimeBin(iArr2));
+    System.out.println("Average Time for LinSearch w/ length 5000000: " + avgTimeLin(iArr2));
+    System.out.println("Diff btwn Bin and Lin w/ length 5000000: " + (avgTimeLin(iArr2)-avgTimeBin(iArr2)));
+
+    System.out.println("-------------------------------------------------");
+
+    //Finding time diff for arr.length = 500000
+    System.out.println("Average Time for BinSearch w/ length 10000000: " + avgTimeBin(iArr3));
+    System.out.println("Average Time for LinSearch w/ length 10000000: " + avgTimeLin(iArr3));
+    System.out.println("Diff btwn Bin and Lin w/ length 10000000: " + (avgTimeLin(iArr3)-avgTimeBin(iArr3)));
+
+  }
+
 }
